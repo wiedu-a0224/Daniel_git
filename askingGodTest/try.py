52a1cae269919321361f00b7c0820c598ff83141
handydaniel <99458123+handydaniel@users.noreply.github.com>
2024-05-29 23:21:11 +0800

    2024-0529-2322

--- askingGodTest/try.py
+++ askingGodTest/try.py
@@ -4,9 +4,9 @@ import random
 # Load the JSON file
 with open('testJson.json') as file:
     data = json.load(file)
+    print(type(data))
+# # Get a random key from the JSON data
+# random_key = random.choice(list(data.keys()))
 
-# Get a random key from the JSON data
-random_key = random.choice(list(data.keys()))
-
-# Print the corresponding value
-print(data[random_key])
+# # Print the corresponding value
+# print(data[random_key])
