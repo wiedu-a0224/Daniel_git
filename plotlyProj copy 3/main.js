c0d28e15e99240576dce8c72579e3eabf3d0e0aa
handydaniel <metavey@gmail.com>
2024-06-18 16:55:05 +0800

    20240618 16:00

--- plotlyProj copy 3/main.js
+++ plotlyProj copy 3/main.js
@@ -2,7 +2,9 @@ d3.csv('https://raw.githubusercontent.com/ryanchung403/dataset/main/train_data_t
 .then(
     res => {
         console.log(res);
-        drawLineChart(res);
+        drawPieChart1(res);
+        drawPieChart2(res);
+        drawPieChart3(res);
     }
 );
 
@@ -12,16 +14,14 @@ function unpack(rows, key) {
      });
 }
 
-function drawPieChart(res) {
+function drawPieChart1(res) {
     
     console.log(unpack(res, "Survived"));
-    console.log(unpack(res, "Sex"));
-    console.log(unpack(res, "Embarked"));
 
 
     let trace1 = {
-        labels : unpack(res, "Survived"),
-        values : unpack(res, "count"),
+        labels : ["Survived", "Not Survived"],
+        values : [],
         type: 'pie',
         title: "Survived vs Not Survived",
         domain: {
@@ -30,40 +30,23 @@ function drawPieChart(res) {
         },
         hole: 0.4
     };
-    let trace2 = {
-        labels : unpack(res, "Sex"),
-        values : unpack(res, "count"),
-        type: 'pie',
-        title: "Male vs Female",
-        domain: {
-            row: 0,
-            column: 1
-        },
-        hole: 0.4
-    }; 
-    let trace3 = {
-        labels : unpack(evaluation_ratio_3, "Embarked"),
-        values : unpack(evaluation_ratio_3, "count"),
-        type: 'pie',
-        title: "科目三",
-        domain: {
-            row: 1,
-            column: 0
-        },
-        hole: 0.4
-    };
-    let trace4 = {
-        labels : unpack(evaluation_ratio_4, "name"),
-        values : unpack(evaluation_ratio_4, "count"),
-        type: 'pie',
-        title: "科目四",
-        domain: {
-            row: 1,
-            column: 1
-        },
-        hole: 0.4
-    };
-    let data = [trace1, trace2, trace3, trace4];
+    let survived = 0;
+    let not_survived = 0;
+    res.forEach(function(d){
+        if(d["Survived"] == 1){
+            survived++;
+        }else{
+            not_survived++;
+        }
+    });
+    console.log(survived);
+    console.log(not_survived);
+    trace1.values.push(survived);
+    trace1.values.push(not_survived);
+    trace1.text = trace1.values;
+
+   
+    let data = [trace1];
 
     let layout = {
         margin:{
@@ -72,11 +55,110 @@ function drawPieChart(res) {
             r: 10,
             b: 80
         },
-        grid:{
-            rows: 2,
-            columns: 2
-        }
+        // grid:{
+        //     rows: 2,
+        //     columns: 2
+        // }
     };
 
-    Plotly.newPlot('myGraph', data , layout);
+    Plotly.newPlot('myGraph1', data , layout);
+}
+function drawPieChart2(res) {
+    
+
+    console.log(unpack(res, "Sex"));
+
+
+
+    let trace1 = {
+        labels : ["Male", "Female"],
+        values : [],
+        type: 'pie',
+        title: "Sex",
+        domain: {
+            row: 0,
+            column: 0
+        },
+        hole: 0.4
+    };
+    let male = 0;
+    let female = 0;
+    res.forEach(function(d){
+        if(d["Sex"] == "male"){
+            male++;
+        }else{
+            female++;
+        }
+    });
+    console.log(male);
+    console.log(female);
+    trace1.values.push(male);
+    trace1.values.push(female);
+    trace1.text = trace1.values;
+
+    let data = [trace1];
+
+    let layout = {
+        margin:{
+            t: 10,
+            l: 30,
+            r: 10,
+            b: 80
+        },
+        // grid:{
+        //     rows: 2,
+        //     columns: 2
+        // }
+    };
+
+    Plotly.newPlot('myGraph2', data , layout);
+}
+function drawPieChart3(res) {
+    
+
+    console.log(unpack(res, "Embarked"));
+
+
+    let trace1 = {
+        labels : ["S", "C"],
+        values : [],
+        type: 'pie',
+        title: "Embarked",
+        domain: {
+            row: 0,
+            column: 0
+        },
+        hole: 0.4
+    };
+    let s = 0;
+    let c = 0;
+    res.forEach(function(d){
+        if(d["Embarked"] == "S"){
+            s++;
+        }else{
+            c++;
+        }
+    });
+    console.log(s);
+    console.log(c);
+    trace1.values.push(s);
+    trace1.values.push(c);
+    trace1.text = trace1.values;
+
+    let data = [trace1];
+
+    let layout = {
+        margin:{
+            t: 10,
+            l: 30,
+            r: 10,
+            b: 80
+        },
+        // grid:{
+        //     rows: 2,
+        //     columns: 2
+        // }
+    };
+
+    Plotly.newPlot('myGraph3', data , layout);
 }
